f78021a825af630ad1f1259b5356e9e056a45c13
Troy Vines <troyvines86@gmail.com>
2025-08-09 17:07:38 -0500

    correct the end point

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ mongoose
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
+
 app.use("/", indexRouter);
 
 app.listen(PORT, () => {
