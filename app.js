feb69e1b1d65460717247a99681d9126fbb00f96
Troy Vines <troyvines86@gmail.com>
2025-08-23 19:29:12 -0500

    app.js error correction

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const indexRouter = require("./routes/index");
 const { createUser, login } = require("./controllers/users");
 
-const app = express();
-const { PORT = 3001 } = process.env;
+const { PORT = 3001, MONGO_URL = "mongodb://127.0.0.1:27017/wtwr_db" } =
+  process.env;
 
-const cors = require("cors");
+const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .connect(MONGO_URL)
   .then(() => console.log("MongoDB connected"))
-  .catch(console.error);
-
-app.use(express.json());
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.post("/signup", createUser);
 app.post("/signin", login);
-
 app.use("/", indexRouter);
+
+app.use((req, res) => {
+  res.status(404).send({ message: "Requested resource not found" });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
